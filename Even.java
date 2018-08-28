7cf5ae06041f34387710d6aa0f048ad41dfc4af8
16ec041 <42169930+16ec041@users.noreply.github.com>
2018-08-28 13:39:11 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,18 @@
+public class Even
+{
+  public ststic void main(String args[])
+  {
+    int a=10;
+    if(a/2)
+    {
+      System.out.println("a is even");
+      System.out.println("a is valid");
+     }
+     else if(a%2)
+     {
+      System.out.println("a is odd");
+      System.out.println("a is valid");
+     }
+     else
+     {
+      System.out.println("a is invalid");
